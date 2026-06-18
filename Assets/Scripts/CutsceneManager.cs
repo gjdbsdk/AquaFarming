edb6179715ba6bd5cfaf683f17c0f7efa7c6d7fa
gjdbsdk <aquahua0409@gmail.com>
2026-06-19 02:24:43 +0900

    🔨 Refactor : 파일명 & 함수이름 동기화

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CutsceneSceneChanger : MonoBehaviour
+public class Cutscenemanager : MonoBehaviour
 {
     void Update()
     {
